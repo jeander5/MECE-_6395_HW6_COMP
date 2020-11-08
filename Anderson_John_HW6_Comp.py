3c7c91f83f4f5d612c20a0ffe79f404e36ae644b
John Anderson <nhojanderson8@gmail.com>
2020-11-07 20:16:27 -0600

    Add files via upload
    I changed a few things....made a single Thomas algorithm function that takes in the diagonal elements and the rhs, it takes in vectors, even though that means a vector of constants.
    
    I thought I had all the errors gone before but some were still there for part B.
    
    Part A is Solid. which makes me think its the F function....but I don't see what else to do with it.
    
    I just cant get part B as accurate right now....SO there is probably some error with filling the rhs input vector.
    
    But this isn't actually due until the 19th so I got plenty of time.

--- Anderson_John_HW6_Comp.py
+++ Anderson_John_HW6_Comp.py
@@ -12,6 +12,8 @@ Created on Thu Nov  5 18:11:56 2020
 #imports
 import math
 import numpy as np
+import time
+import matplotlib.pyplot as plt
 from math import sin as sin
 from math import cos as cos
 
@@ -23,14 +25,8 @@ from math import cos as cos
 L=math.pi
 D=0.1
 T=10
-F=0
-g_0=0
-g_L=0
-
 #k is just an integer
 k=1
-     
-#functions
 
 #discretize the interval function, for time and length     
 def DIF(L ,N):
@@ -38,58 +34,25 @@ def DIF(L ,N):
     h = L/(N+1)
     x = np.linspace(0, L, N+2)
     return(x, h)
-
-# Thomas Algorithm Function, for part a Crank Nicholson Scheme part a
-#lets try to be smarter about this compared to last time, add only what I need in the function
-
-def thomas_alg_a(N,a,b,c,d,f):
-    """returns approximate u values for the function from Part 1"""
-#inputs are N interavl length, the tridiagonal elements, which are scalars here
-#and f, which is the just the right hand side, or the u_n elements from Crank Nicolson
     
-#Pre Thomas Algorithm set up. For this problem these values are all constant
+def thomas_alg_func(a,b,c,f):
+    """solves tridiagonal matrix"""
+#vectors containing the tridiagonal elements and right hand side
+    N=len(a)
+    u_appx = [0]*N
     alpha = [0]*N
     g = [0]*N
-    u_appx = [0]*N
 #Following the pseudocode
 #Zeroth element of this list corresponds to the first subscript in Thomas Algorithm
-    alpha[0] = a
-    g[0] = -b*f[0]+d*f[1]-c*f[2]
+    alpha[0] = a[0]
+    g[0] = f[0]
     for j in range(1, N):
-        rhs=-b*f[j]+d*f[j+1]-c*f[j+2]
-        alpha[j] = a-(b/alpha[j-1])*c
-        g[j] = rhs-(b/alpha[j-1])*g[j-1]
+        alpha[j] = a[j]-(b[j]/alpha[j-1])*c[j-1]
+        g[j] = f[j]-(b[j]/alpha[j-1])*g[j-1]
     u_appx[N-1] = g[N-1]/alpha[N-1]
     for j in range(1, N):
-        u_appx[-1-j] = (g[-1-j]-c*u_appx[-j])/alpha[-1-j]
-    return u_appx
-
-def thomas_alg_b(N,a,b,c,d,f,F,g0_b):
-    """returns approximate u values for the function from Part 1"""
-#inputs are N interavl length, the tridiagonal elements, which are scalars here
-#and f, which is the just the right hand side, or the u_n elements from Crank Nicolson
-#also in this function we have the prescribed function F which contributes to the rhs
-#and the boundary condition g0_b (u x=0) which contributes to the first rhs equation    
-    
-    alpha = [0]*N
-    g = [0]*N
-    u_appx = [0]*N
-#Following the pseudocode
-#Zeroth element of this list corresponds to the first subscript in Thomas Algorithm
-    alpha[0] = a
-    g[0] = -b*f[0]+d*f[1]-c*f[2]+F[1]-g0_b*b
-    for j in range(1, N):
-        rhs=-b*f[j]+d*f[j+1]-c*f[j+2]+F[1+j]
-        alpha[j] = a-(b/alpha[j-1])*c
-        g[j] = rhs-(b/alpha[j-1])*g[j-1]
-    u_appx[N-1] = g[N-1]/alpha[N-1]
-    for j in range(1, N):
-        u_appx[-1-j] = (g[-1-j]-c*u_appx[-j])/alpha[-1-j]
-    return u_appx
-
-#I will maybe make these 1 function with the same inputs later.
-#right now they are seperate because of the different boundary conditions
-#and the different values for prescribed function F
+        u_appx[-1-j] = (g[-1-j]-c[-1-j]*u_appx[-j])/alpha[-1-j]
+    return u_appx    
 
 #u exact function part a
 def u_exact_func_a(k, D, t, x):
@@ -117,14 +80,15 @@ def u_exact_func_b(k, D, w, t, x):
         func_vals[n,:]=[sin(w*t[n])*cos(k*x) for x in x]
     return func_vals
 
-def BC_Partb(x,t,w):
+def BC_Partb(t,w):
     """returns boundary conditions for the function from Part b"""
 #inputs are x and t list, and then weq which is the vlaue that w*dt is equal to
 #or I can just input w because I need it later for exact value    
 #    dt = t[-1]-t[-2]
 #    w = weq/dt
+    a=cos(k*L)
     g_0b =[sin(w*t) for t in t]
-    g_Lb= [sin(w*t)*cos(k*L) for t in t]
+    g_Lb= [sin(w*t)*a for t in t]
     return(g_0b, g_Lb)
     
 #The prescribed function F
@@ -138,10 +102,20 @@ def preF(D,k,w,x,t):
         func_vals[n,:]=[cos(k*x)*((w*cos(w*t[n])+D*k*k*sin(w*t[n]))) for x in x]
     return func_vals
 
+def avg_error (exact,appx):
+    """returns average error"""
+#inputs are just single row for exact and approximate solution
+#the entire x domain at a single time    
+    N=len(exact)
+    mysum =0
+    for j in range(1,N-1):
+        mysum=mysum+abs((appx[j]-exact[j])/exact[j])
+    error=mysum/N
+    return error
 
 
 # N right here for now, just using the same for x and T
-N = 5
+N = 666
 
 #calling the DIF
 x, dx = DIF(L,N)
@@ -151,25 +125,7 @@ w=0.1/dt
 len_x = len(x)
 len_t = len(t)
 
-#part a
-
-#zeros u_approximation matrix
-#we are gonna fill this baby up as we go
-u_appx=np.zeros(shape=(len_t, len_x))
-
-#rows are time steps, columns x position
-#filling in initial and boundary conditions
-# the boundary conditions u(x=0,t) and  u(x=L,t) are zero for all t  so I just leave as zeros
-# this will need to be modified for part b
-# nah lets just right it generally right now
-
-#Boundary Conditions
-u_appx[:,0]=g_0
-u_appx[:,-1]=g_L
-#Initial Conditions
-f=[sin(k*x) for x in x]
-u_appx[0,1:-1]=f[1:-1]
-#I still have [1:-1] no need to reassign those endpoints for t =0
+#and I will define the CN constants right here as well since I have dx and dt
 
 #constants from the CN scheme used in the thomas algorithm
 #i know how to spell lambda, put python has lambda functions so I write lamda
@@ -180,42 +136,87 @@ c=-lamda/2
 #im defining a d so it doesnt have to calculate inside loops or functions
 d=1-lamda
 
-#im not really seeing a better way to do this besides calling that function inside a for loop and
-#filling up my u_appx matrix
+#Part a
+#values given in problem statement
+g_0=0
+g_L=0
+f=[sin(k*x) for x in x]
+F= np.zeros(shape=(len_t, len_x))
+
+#solution matrix, will be filled in
+u_appx_a=np.zeros(shape=(len_t, len_x))
+
+#Boundary Conditions
+u_appx_a[:,0]=g_0
+u_appx_a[:,-1]=g_L
+#Initial Conditions
+u_appx_a[0,1:-1]=f[1:-1]
+#I still have [1:-1] no need to reassign those endpoints for t =0
+
+#input vecotrs for the thomas algorithm function
+av= [a]*N
+bv= [b]*N
+cv= [c]*N
+rhs= [0]*N
+
+#for loop for calling the thomas algorithm at the different time steps
 for n in range (1,len_t):
-    #I wonder if its inefficient to have u_appx[n-1,: in the function call, I bet it is.
-    #I will just define a new variable    
-    q=u_appx[n-1,:]
-    u_appx[n,1:-1]=thomas_alg_a(N,a,b,c,d,q)
-   
+    Q=F[n-1,:]
+    q=u_appx_a[n-1,:]   
+    rhs[0]=-b*q[0]+d*q[1]-c*q[2]-b*u_appx_a[n,0]+dt*Q[0]
+#    inner for loop for filling up the rhs vector for the thomas algorithm    
+    for j in range (1,N):
+        rhs[j]=-b*q[j]+d*q[j+1]-c*q[j+2]+dt*Q[j+1]
+    u_appx_a[n,1:-1]=thomas_alg_func(av,bv,cv,rhs)
+
 #calling exact function
-u_exact=u_exact_func_a(k, D, t, x)
+u_exact_a=u_exact_func_a(k, D, t, x)
+#calling the error function
+error_a=avg_error(u_exact_a[-1,:],u_appx_a[-1,:])
 
 
-#part b
+# =============================================================================
+# part B
+# =============================================================================
+#is not working
 
-#set up solution matrix    
-u_appx_b=np.zeros(shape=(len_t, len_x))
-#fill in boundary conditions, need to call that function.
-g0_b,gL_b=BC_Partb(x,t,w)
-u_appx_b[:,0]=g0_b
-u_appx_b[:,-1]=gL_b
-
-#The Initial conditions are just zero so no need to reassign them
-#calling the prescribed function F
+#values given in problem statement
+g_0,g_L=BC_Partb(t,w)
+f=[0]*len_x
 F=preF(D,k,w,x,t)
-# the constants a,b,c,d are the same for part 1
 
-#filling up my matrix
+#solution matrix, will be filled in
+u_appx_b=np.zeros(shape=(len_t, len_x))
+
+#Boundary Conditions
+u_appx_b[:,0]=g_0
+u_appx_b[:,-1]=g_L
+#Initial Conditions
+u_appx_b[0,:]=f
+
+#input vecotrs for the thomas algorithm function
+#i dont really need to redine these but im trying to be consistent
+av= [a]*N
+bv= [b]*N
+cv= [c]*N
+rhs= [0]*N
+
+#for loop for calling the thomas algorithm at the different time steps
 for n in range (1,len_t):
+    Q=F[n-1,:]
     q=u_appx_b[n-1,:]
-    #that prescribed function F needs to be multiplied by dt
-    Q=F[n-1,:]*dt
-    u_appx_b[n,1:-1]=thomas_alg_b(N,a,b,c,d,q,Q,u_appx_b[n,0])
+    rhs[0]=-b*q[0]+d*q[1]-c*q[2]-b*u_appx_b[n,0]+dt*Q[0]
+#    inner for loop for filling up the rhs vector for the thomas algorithm
+    for j in range (1,N):
+        rhs[j]=-b*q[j]+d*q[j+1]-c*q[j+2]+dt*Q[j+1]
+    u_appx_b[n,1:-1]=thomas_alg_func(av,bv,cv,rhs)
 
 #calling exact function
-u_exact_b=u_exact_func_b(k, D, w, t, x)    
+u_exact_b=u_exact_func_b(k, D, w, t, x)
+#calling the error function
+error_b=avg_error(u_exact_b[-1,:],u_appx_b[-1,:])
 
-#next I need to do error and grid convergence. For the grid convergence I dont think I will do it 
-#in a while loop like for HW 4, I will just do it manually and save the graphs, tables ect.
-#its already pretty close with not that many grid points
+
+#I dont know maybe it is right. as long as N is even I dont have that humongous error from the x/sin(pi/2) term
+#error goes down with with Increaing N
+#and error goes up with increasing omega
